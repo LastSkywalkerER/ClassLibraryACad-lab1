c0833b1436b49be00c4ce727f824031608eef252
LastSkywalkerER <maxdr1998@gmail.com>
2021-06-29 17:46:16 +0300

    Добавьте файлы проекта.

---
+++ Class1.cs
@@ -0,0 +1,16 @@
+﻿using Autodesk.AutoCAD.Runtime;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.ApplicationServices;
+
+namespace ClassLibraryACad
+{
+    public class Class1
+    {
+        [CommandMethod("HelloWorld")]
+        public void helloWorld()
+        {
+            Editor editor = Application.DocumentManager.MdiActiveDocument.Editor;
+            editor.WriteMessage("Hello World");
+        }
+    }
+}
