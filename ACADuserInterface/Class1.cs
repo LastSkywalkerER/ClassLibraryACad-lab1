1fbffe1d3490874ee8f86ef663d820faf91f1f95
LastSkywalkerER <maxdr1998@gmail.com>
2021-07-08 22:10:00 +0300

    add some code

--- ACADuserInterface/Class1.cs
+++ ACADuserInterface/Class1.cs
@@ -1,8 +1,27 @@
-﻿using Autodesk.AutoCAD.Windows;
+﻿using Autodesk.AutoCAD.Runtime;
+using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.Geometry;
+
+using Autodesk.AutoCAD.Windows;
+
+using System.Runtime.InteropServices;
 
 namespace ACADuserInterface
 {
-    public class adskClass
-    {
-    }
+	[Assembly: ExtensionApplication(GetType(adskClass))]
+	public class adskClass : IExtensionApplication
+	{
+		ContextMenuExtension myContextMenu;
+		public void Initialize()
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public void Terminate()
+		{
+			throw new System.NotImplementedException();
+		}
+	}
 }
